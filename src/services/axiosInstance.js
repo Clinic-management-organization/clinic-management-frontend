5e2d372e994cb619ae2d9a6391ee9e56020fb200
ameni-selmi <ameniselmi456@gmail.com>
2024-01-16 02:38:14 +1300

    commit

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://127.0.0.1:8080/',
   headers: {
     common: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      Authorization: `${localStorage.getItem('token')}`,
     },
   },
 });
