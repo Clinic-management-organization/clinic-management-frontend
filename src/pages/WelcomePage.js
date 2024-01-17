54feff1cda2f0cf39c61113df382a1e55fc80305
Nourchene <hamritanourchene@gmail.com>
2024-01-17 20:51:38 +0100

    fix pages

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -3,24 +3,25 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { Toaster } from "react-hot-toast";
 import "./Welcome.css";
+import { Container } from "@mui/material";
 //const theme = createTheme();
 
 export default function WelcomePage() {
   return (
-    <div >
+    <Container className="container" component="main" style={{ width: "70%" }}>
       <Toaster />
       <CssBaseline />
 
       <div className="anneeStatusContainer">
-        <img className="statusimg"  alt="" />
+        <img src ="https://img.freepik.com/vecteurs-libre/medecins-analyses-prescriptives-personnalisees_335657-1882.jpg?w=996&t=st=1705258337~exp=1705258937~hmac=9a74dd2843da73d6ac9f8c5b6383ce4b86a5ee44d41d8caaa465ba1809f65d06)"className="statusimg"  alt="" />
         <div className="msg">
           <h2 className="text1">
-          Welcome <span class="en_attente">Clinique...</span>
+          Bienvenue <span class="en_attente"> Dans Notre Clinique...</span>
           </h2>
 
           
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
